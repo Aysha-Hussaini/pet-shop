666ece79d2d0ee391f61e3512e8f5564f86c52a7
Aysha-Hussaini <aysha.mqsd@gmail.com>
2021-12-14 19:21:44 +0530

    Final Commit

--- proof-of-existence/truffle-config.js
+++ proof-of-existence/truffle-config.js
@@ -1,16 +1,22 @@
-const HDWallet = require('truffle-hdwallet-provider');
-const infuraKey = 'fj4jll3k.....';
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = 'e2bab8b3672741d6b2e5059eb0f29781';
  
-const fs = require('fs');
-const mnemonic = fs.readFileSync('.secret').toString().trim();
 
+const mnemonic = "present step dutch mule meadow race drip spider genre make drama cherry";
+const infuraURL = 'https://rinkeby.infura.io/v3/e2bab8b3672741d6b2e5059eb0f29781';
 
 module.exports = {
   networks: {
-      development: {
-          host: 'localhost',
-          port: 8545,
-          network_id: '*'
-      }
+    development: {
+        host: 'localhost',
+        port: 8545,
+        network_id: '*'
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,          // Rinkeby's network id
+      gas: 5500000,        
+    },
   }
 };
